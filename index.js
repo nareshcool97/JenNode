6a54caab41114a23512637e59c22b5b5e5e88dc2
Naresh Ausula <naresh.ausula@sumtotalsystems.com>
2018-12-28 10:23:45 +0530

    Sample code node js

---
+++ index.js
@@ -0,0 +1,12 @@
+//importing node framework
+var express = require('express');
+ 
+let app = express();
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', (req, res) => {
+ res.send('hello world');
+});
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+var request = require('supertest');
+var app = require('../index.js');
+describe('GET /', () => {
+ it('respond with hello world', done => {
+ //navigate to root and check the the response is "hello world"
+ request(app).get('/').expect('hello world', done);
+ });
+});
